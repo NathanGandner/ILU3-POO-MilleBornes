1f37ff14203493b3fb4df5528dad3c759cafda8c
NathanGandner <125391798+NathanGandner@users.noreply.github.com>
2024-11-06 16:43:35 +0100

    Add files via upload

--- MilleBornes/src/jeu/ZoneDeJeu.java
+++ MilleBornes/src/jeu/ZoneDeJeu.java
@@ -33,7 +33,7 @@ public class ZoneDeJeu extends Carte {
 		{
 			return 200;
 		}
-		if (head(lim).getClass() == DebutLimite.class)
+		if (lim.get(lim.size()-1) instanceof DebutLimite)
 		{
 			return 50;
 		}
@@ -50,68 +50,61 @@ public class ZoneDeJeu extends Carte {
 	public void deposer(Carte carte) {
 		if(carte == null)
 			return;
-		if(carte.getClass() == (Borne.class)) {
-			System.out.println("ENFIIIINNN");
+		if(carte instanceof Borne) {
 			bor.add((Borne)carte);
 		}
-		if(carte.getClass() == FinLimite.class)
+		if(carte instanceof FinLimite)
 			lim.add((FinLimite)carte);
-		if(carte.getClass() == DebutLimite.class)
+		if(carte instanceof DebutLimite)
 			lim.add((DebutLimite)carte);
-		if(carte.getClass() == Bataille.class)
+		if(carte instanceof Bataille)
 			bat.add((Bataille)carte);
-		System.out.println("----");
-		System.out.println("dépôt impossible dans la classe ZoneDeJeu");
-		System.out.println("class de la carte: "+ carte.getClass());
-		System.out.println("----");
 	}
 	
 	public boolean peutAvancer() {
-		return !(bat.isEmpty() || !equals(bat.get(bat.size()), new Parade(Type.FEU)));
+		return !bat.isEmpty() && equals(bat.get(bat.size()-1), new Parade(Type.FEU));
 	}
 	
 	private boolean estDepotFeuVertAutorise() {
-		return (bat.isEmpty() || equals(bat.get(bat.size()), new Attaque(Type.FEU)) || (bat.get(bat.size()).getClass() == Parade.class && !equals(bat.get(bat.size()),new Parade(Type.FEU))));
+		return (bat.isEmpty() || equals(bat.get(bat.size()-1), new Attaque(Type.FEU)) || (bat.get(bat.size()-1) instanceof Parade && !equals(bat.get(bat.size()-1),new Parade(Type.FEU))));
 	}
 	
 	private boolean estDepotBorneAutorise(Borne borne) {
-		return (peutAvancer() || borne.getKm()<=donnerLimitationVitesse() || donnerKmParcourus()+borne.getKm()<=1000);
+		return (peutAvancer() && borne.getKm()<=donnerLimitationVitesse() && donnerKmParcourus()+borne.getKm()<=1000);
 	}
 	
 	private boolean estDepotLimiteAutorise(Limite limite) {
 		if (equals(limite, new DebutLimite())){
 			return (lim.isEmpty() || equals(lim.get(lim.size()), new FinLimite()));
 		} else {
-			return (!lim.isEmpty() && equals(lim.get(lim.size()), new DebutLimite()));
+			return (!lim.isEmpty() && equals(lim.get(lim.size()-1), new DebutLimite()));
 			
 		}
 	}
 	
 	private boolean estDepotBatailleAutorise(Bataille bataille) {
-		if(bataille.getClass() == Attaque.class) {
+		if(bataille instanceof Attaque) {
 			return peutAvancer();
 		} else {
 			if(equals(bataille, new Parade(Type.FEU))) {
 				return estDepotFeuVertAutorise();
 			} else {
-				return (!bat.isEmpty() && bataille.getType() == bat.get(bat.size()).getType());
+				return (!bat.isEmpty() && bataille.getType() == bat.get(bat.size()-1).getType());
 			}
 		}
 	}
-	
+
 	public boolean estDepotAutorise(Carte carte) {
 		if(carte == null)
 			return false;
-		if(carte.getClass() == Borne.class)
+		if(carte instanceof Borne)
 			return estDepotBorneAutorise((Borne) carte);
-		if(carte.getClass() == Limite.class)
+		if(carte instanceof Limite)
 			return estDepotLimiteAutorise((Limite) carte);
-		if(carte.getClass() == Bataille.class)
+		if(carte instanceof Bataille)
 		{
-			System.out.println("IIICCCCIIIII");
 			return estDepotBatailleAutorise((Bataille) carte);
 		}
-		System.out.println("LA :" + carte.getClass());
 		return false;
 	}
 }
